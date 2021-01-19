1267766528f545c0991c82879a8f0be268c8858a
dedyksuntoro <39944254+dedyksuntoro@users.noreply.github.com>
2021-01-19 16:13:01 +0700

    Validation & Login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
 $router->group(['middleware' => 'auth'], function() use($router) {
     $router->get('/users', 'UserController@index');
     $router->post('/users', 'UserController@store');
@@ -21,3 +22,5 @@ $router->group(['middleware' => 'auth'], function() use($router) {
     $router->put('/users/{id}', 'UserController@update');
     $router->delete('/users/{id}', 'UserController@destroy');
 });
+
+$router->post('/login', 'UserController@login');
